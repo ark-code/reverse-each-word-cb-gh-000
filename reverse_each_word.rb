15d5c47e001709be7bdfb78881d409dbaca08884
Akshay K <akshaykunti1@gmail.com>
2020-01-03 11:32:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,7 @@
+
 def reverse_each_word(str)
   str = str.split(' ')
-  new = ''
-  str.each do |e|
-    new = new +' '+e.reverse
+  str.collect do |e|
+    e.reverse
   end
-  return new.strip
 end
-
